1dbf3edb7d3a452ab026f3b3ccc4ff90fd268e01
alison-andrade <sidneialison@gmail.com>
2026-05-31 01:01:33 -0300

    feat: layout inicial pagina de registro

---
+++ app/register/page.tsx
@@ -0,0 +1,11 @@
+export default function Page() {
+    return (
+        <main className="flex items-center justify-center min-h-screen p-4 bg-gray-100 dark:bg-gray-800 dark:text-white">
+            <div className="relative mx-auto w-full max-w-md flex-col space-y-2.5 md:-mt-32">
+                {/* <Suspense>
+                    <RegisterForm />
+                </Suspense> */}
+            </div>
+        </main>
+    );
+}
